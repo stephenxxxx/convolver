40df4dc634a73d20c152f3ade18bc2a2c82d5556
nobody <nobody@61397ecf-7810-0410-9c1b-9d4d218aaa15>
2005-09-12 22:47:56 +0000

    This commit was manufactured by cvs2svn to create tag 'convolver1-25'.

---
+++ tags/convolver1-25/convolver/comparewavs/comparewavs.cpp
@@ -0,0 +1,244 @@
+// Convolver: DSP plug-in for Windows Media Player that convolves an impulse respose
+// filter it with the input stream.
+//
+// Copyright (C) 2005  John Pavel
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+//////////////////////////////////////////////////////////////////////////////////
+//
+// comparewavs.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "Common\dxstdafx.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+	HRESULT hr = S_OK;
+
+	if (argc != 6)
+	{
+		_tprintf(TEXT("Usage: comparewavs inputwavfile1.wav skipblocks1 inputwavfile2.wav skipblocks2 differencewavfilename.wav\n"));
+		return 1;
+	}
+
+	CWaveFile* WavF1 = new CWaveFile;
+	CWaveFile* WavF2 = new CWaveFile;
+
+	if( FAILED( hr = WavF1->Open( argv[1], NULL, WAVEFILE_READ ) ) )
+	{
+		_tprintf(TEXT("Failed to open %s for reading\n"), argv[1]);
+		goto exit;
+	}
+
+	if( FAILED( hr = WavF2->Open( argv[3], NULL, WAVEFILE_READ ) ) )
+	{
+		_tprintf(TEXT("Failed to open %s for reading\n"), argv[3]);
+		goto exit;
+	}
+
+	if ( WavF1->GetFormat()->nAvgBytesPerSec != WavF2->GetFormat()->nAvgBytesPerSec)
+	{
+		_tprintf(TEXT("AvgBytesPerSec not equal: %i, %i\n"), WavF1->GetFormat()->nAvgBytesPerSec, WavF2->GetFormat()->nAvgBytesPerSec);
+		hr = 1;
+		goto exit;
+	}
+
+	if ( WavF1->GetFormat()->nBlockAlign != WavF2->GetFormat()->nBlockAlign)
+	{
+		_tprintf(TEXT("BlockAlign not equal: %i, %i\n"), WavF1->GetFormat()->nBlockAlign, WavF2->GetFormat()->nBlockAlign);
+		hr = 1;
+		goto exit;
+	}
+
+	if ( WavF1->GetFormat()->nChannels != WavF2->GetFormat()->nChannels)
+	{
+		_tprintf(TEXT("Channels not equal: %i, %i\n"), WavF1->GetFormat()->nChannels, WavF2->GetFormat()->nChannels);
+		hr = 1;
+		goto exit;
+	}
+
+	if ( WavF1->GetFormat()->nChannels != WavF2->GetFormat()->nChannels)
+	{
+		_tprintf(TEXT("Channels not equal: %i, %i\n"), WavF1->GetFormat()->nChannels, WavF2->GetFormat()->nChannels);
+		hr = 1;
+		goto exit;
+	}
+
+	if ( WavF1->GetFormat()->nSamplesPerSec != WavF2->GetFormat()->nSamplesPerSec)
+	{
+		_tprintf(TEXT("SamplesPerSec not equal: %i, %i\n"), WavF1->GetFormat()->nSamplesPerSec, WavF2->GetFormat()->nSamplesPerSec);
+		hr = 1;
+		goto exit;
+	}
+
+	if ( WavF1->GetFormat()->wBitsPerSample != WavF2->GetFormat()->wBitsPerSample)
+	{
+		_tprintf(TEXT("BitsPerSample not equal: %i, %i\n"), WavF1->GetFormat()->wBitsPerSample, WavF2->GetFormat()->wBitsPerSample);
+		hr = 1;
+		goto exit;
+	}
+
+	if ( WavF1->GetFormat()->wFormatTag != WavF2->GetFormat()->wFormatTag)
+	{
+		_tprintf(TEXT("FormatTag not equal: %i, %i\n"), WavF1->GetFormat()->wFormatTag, WavF2->GetFormat()->wFormatTag);
+		hr = 1;
+		goto exit;
+	}
+
+	CWaveFile* WavDiff = new CWaveFile;
+	if (FAILED(hr = WavDiff->Open(argv[5], WavF1->GetFormat(), WAVEFILE_WRITE)))
+	{
+		_tprintf(TEXT("Failed to open %s for writing\n"), argv[5]);
+		goto exit;
+	}
+
+#ifdef UNICODE 
+	CHAR strTmp[2*(sizeof(10)+1)];	// SIZE equals (2*(sizeof(tstr)+1)). This ensures enough
+	// room for the multibyte characters if they are two
+	// bytes long and a terminating null character.
+
+	wcstombs(strTmp, (const wchar_t *) argv[2], sizeof(strTmp)); 
+	int nSkipBlocks1 =  atoi(strTmp);
+	wcstombs(strTmp, (const wchar_t *) argv[4], sizeof(strTmp)); 
+	int nSkipBlocks2 =  atoi(strTmp); 
+
+#else 
+
+	int nSkipBlocks1 = atoi(argv[2]);
+	int nSkipBlocks2 = atoi(argv[4]);
+
+#endif
+
+	if ( WavF1->GetSize() != WavF2->GetSize() )
+	{
+		_tprintf(TEXT("File sizes not equal: %i, %i\n"), WavF1->GetSize(), WavF2->GetSize());
+	}
+
+
+	DWORD dwSizeRead = 0;
+
+	WavF1->ResetFile();
+	WavF2->ResetFile();
+
+	switch (WavF1->GetFormat()->wFormatTag)
+	{
+	case WAVE_FORMAT_PCM:
+		_tprintf(TEXT("PCM WAV format not supported\n"));
+		hr =  E_NOTIMPL; // TODO: Unimplemented filter type
+		break;
+
+	case WAVE_FORMAT_IEEE_FLOAT:
+		{
+			switch (WavF1->GetFormat()->wBitsPerSample)
+			{
+			case sizeof(float) * 8: //32
+				{
+					DWORD dwSampleSize = sizeof(float);
+					UINT nSizeWrote = 0;
+					float fSample1 = 0;
+					float fSample2 = 0;
+					float fSampleDiff = 0;
+
+					for (int block=0; block < min(nSkipBlocks1, WavF1->GetSize() / WavF1->GetFormat()->nBlockAlign);++block)
+					{
+						for (int channel=0; channel < WavF1->GetFormat()->nChannels; channel++)
+						{
+							if (hr = FAILED(WavF1->Read((BYTE*)&fSample1, dwSampleSize, &dwSizeRead)))
+							{
+								_tprintf(TEXT("Failed to inital read sample from %s\n"), argv[1]);
+								goto exit;
+							}
+						}
+					}
+
+					for (int block=0; block < min(nSkipBlocks2, WavF1->GetSize() / WavF1->GetFormat()->nBlockAlign);++block)
+					{
+						for (int channel=0; channel < WavF1->GetFormat()->nChannels; channel++)
+						{
+							if (hr = FAILED(WavF2->Read((BYTE*)&fSample2, dwSampleSize, &dwSizeRead)))
+							{
+								_tprintf(TEXT("Failed to read initial sample from %s\n"), argv[1]);
+								goto exit;
+							}
+						}
+					}
+
+					for (int block=max(nSkipBlocks1, nSkipBlocks2); block < (WavF1->GetSize() / WavF1->GetFormat()->nBlockAlign);++block)
+					{
+						for (int channel=0; channel <WavF1->GetFormat()->nChannels; channel++)
+						{
+							if (hr = FAILED(WavF1->Read((BYTE*)&fSample1, dwSampleSize, &dwSizeRead)))
+							{
+								_tprintf(TEXT("Failed to read sample from %s\n"), argv[1]);
+								goto exit;
+							}
+							if (dwSampleSize != dwSizeRead)
+							{
+								_tprintf(TEXT("Failed to read sample from %s\n"), argv[1]);
+								fSample1 = 0;
+							}
+
+							if (hr = FAILED(WavF2->Read((BYTE*)&fSample2, dwSampleSize, &dwSizeRead)))
+							{
+								_tprintf(TEXT("Failed to read sample from %s\n"), argv[3]);
+								goto exit;
+							}
+							if (dwSampleSize != dwSizeRead)
+							{
+								_tprintf(TEXT("Failed to read sample from %s\n"), argv[3]);
+								fSample2 = 0;
+							}
+
+							fSampleDiff = fSample1 - fSample2;
+
+							if (FAILED(hr = WavDiff->Write(dwSampleSize, (BYTE *)&fSampleDiff, &nSizeWrote)))
+							{
+								_tprintf(TEXT("Failed to write sample to %s\n"), argv[5]);
+								goto exit;
+							};
+
+#ifdef _DEBUG
+							_tprintf(TEXT("%i,%i, %.6g, %.6g, %.6g\n"), block, channel, fSample1, fSample2, fSampleDiff);
+#endif
+						}
+					}
+				}
+				break;
+			default:
+				_tprintf(TEXT("Unsupported IEEE float format\n"));
+				hr =  E_NOTIMPL;
+				break;
+			}
+		}
+		break;
+	default:
+		_tprintf(TEXT("Unsupported WAV file format\n"));
+		hr = E_NOTIMPL;
+		break;
+	}
+
+exit:
+	if (hr)
+		_tprintf(TEXT("Error code: 0x%x\n"), hr);
+
+	SAFE_DELETE(WavF1);
+	SAFE_DELETE(WavF2);
+	SAFE_DELETE(WavDiff);
+	return hr;
+}
+
---
+++ tags/convolver1-25/convolver/perftest/perftest.cpp
@@ -0,0 +1,131 @@
+// perftest.cpp	: Defines the entry	point for the console application.
+//
+
+#include "stdafx.h"
+
+#include "Common\dxstdafx.h"
+#include <sstream>
+#if defined(DEBUG) | defined(_DEBUG)
+#include "debugging\debugging.h"
+#endif
+#include "debugging\fasttiming.h"
+#include "convolution\wavefile.h"
+#include "convolution\convolution.h"
+
+int	_tmain(int argc, _TCHAR* argv[])
+{
+
+	HRESULT	hr = S_OK;
+	Holder<CConvolution> conv;
+	double fElapsedLoad	= 0;
+	double fTotalElapsedLoad = 0;
+	double fElapsedCalc = 0;
+	double fTotalElapsedCalc = 0;
+	apHiResElapsedTime t;
+
+	CWaveFileHandle	FilterWav;
+
+#if	defined(DEBUG) | defined(_DEBUG)
+	debugstream.sink (apDebugSinkConsole::sOnly);
+#endif
+
+	if (argc !=	4)
+	{
+		std::wcerr << "Usage: perftest MaxnPartitions nIterations filter.wav" << std::endl;
+		return 1;
+	}
+
+	try
+	{
+		std::wistringstream	szPartitions(argv[1]);
+		int max_nPartitions;
+		szPartitions >>	max_nPartitions;
+
+		if (max_nPartitions	< 0 || max_nPartitions > 1000)
+		{
+			std::cerr << "max_nPartitions (" << max_nPartitions << ") must be between 0 and 1000" << std::endl;
+			throw(std::length_error("invalid max_nPartitions"));
+		}
+
+		std::wistringstream	szIterations(argv[2]);
+		int nIterations;
+		szIterations >>	nIterations;
+		if (nIterations	< 1 || nIterations > 1000)
+		{
+			std::wcerr << "nIterations (" << nIterations << ") must be between 1 and 1000" << std::endl;
+			throw(std::length_error("invalid nIterations"));
+		}
+
+#ifdef LIBSNDFILE
+		SF_INFO sfinfo;
+		ZeroMemory(&sfinfo, sizeof(SF_INFO));
+		CWaveFileHandle FilterWav(argv[3], SFM_READ, &sfinfo);
+		std::cerr << waveFormatDescription(sfinfo, 	"Filter file format: ") << std::endl;
+#else
+		std::basic_string< _TCHAR >FilterFileName(argv[3],	_tcslen(argv[3]));
+		if(	FAILED(	hr = FilterWav->Open( argv[3], NULL, WAVEFILE_READ ) ) )
+		{
+			std::wcerr << "Failed to open "	<< FilterFileName << " for reading"	<< std::endl;
+			throw(hr);
+		}
+
+		WAVEFORMATEX *pWave	= FilterWav->GetFormat();
+		std::cerr << waveFormatDescription(reinterpret_cast<WAVEFORMATEXTENSIBLE*>(pWave), 
+			FilterWav->GetSize() / FilterWav->GetFormat()->nBlockAlign,	"Filter file format: ") << std::endl;
+#endif
+
+		float fAttenuation	= 0;
+
+		std::cout << std::endl << "Partitions\tSecCalc\tSecLoad\tAttenuation\tFilter Length\tPartition Length\tIteration" << std::endl;
+
+		for (int nPartitions = 0; nPartitions <= max_nPartitions; ++nPartitions)
+		{
+			for (int nIteration = 1; nIteration<=nIterations; ++nIteration)
+			{
+
+				t.reset();
+#ifdef LIBSNDFILE
+				conv = new Cconvolution_ieeefloat(argv[3], nPartitions == 0 ? 1 : nPartitions); // Sets conv.	nPartitions==0 => use overlap-save
+#else
+				hr = SelectConvolution(pWave, conv,	argv[3], nPartitions ==	0 ?	1 :	nPartitions); // Sets conv.	nPartitions==0 => use overlap-save
+				if (FAILED(hr))
+				{
+					std::wcerr << "Failed to select filter " << FilterFileName << std::endl;
+					throw(hr);
+				}
+#endif
+				fElapsedLoad = t.sec();
+				fTotalElapsedLoad += fElapsedLoad;
+				
+				t.reset();
+				hr = calculateOptimumAttenuation(fAttenuation, argv[3],	nPartitions);
+				fElapsedCalc = t.sec();
+				fTotalElapsedCalc += fElapsedCalc;
+				
+				if (FAILED(hr))
+				{
+					std::wcerr << "Failed to calculate optimum attenuation"	<< std::endl;
+					throw (hr);
+				}
+				std::cout  << nPartitions << "\t"  << fElapsedCalc << "\t" << fElapsedLoad << "\t" 
+					<< fAttenuation << "\t" << conv->FIR.nFilterLength << "\t" 
+					<< conv->FIR.nPartitionLength << "\t" << nIteration  << std::endl;
+			}
+		}
+
+		std::cout << std::endl << "Total load time: " << fTotalElapsedLoad  << "s" << std::endl;
+		std::cout << "Total execution time: " << fTotalElapsedCalc << "s" << std::endl;
+
+	}
+	catch (...)
+	{
+		std::wcerr << "Failed" <<std::endl;
+	}
+
+#if defined(DEBUG) | defined(_DEBUG)
+	_CrtDumpMemoryLeaks();
+#endif
+
+	return hr;
+
+}
