17beaf689bb767d0edbbb7b507de1bacc8a6104c
godrick <godrick@61397ecf-7810-0410-9c1b-9d4d218aaa15>
2005-10-02 13:16:25 +0000

    *** empty log message ***

--- trunk/convolver/perftest/stdafx.cpp
+++ trunk/convolver/perftest/stdafx.cpp
@@ -1,5 +1,5 @@
 // stdafx.cpp : source file that includes just the standard includes
-// perftest.pch will be the pre-compiled header
+// libsndfile.pch will be the pre-compiled header
 // stdafx.obj will contain the pre-compiled type information
 
 #include "stdafx.h"
