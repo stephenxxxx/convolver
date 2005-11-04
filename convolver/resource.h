81364bed1b5712b75490c6dae65b4f1553bbe102
nobody <nobody@61397ecf-7810-0410-9c1b-9d4d218aaa15>
2005-11-04 00:34:05 +0000

    This commit was manufactured by cvs2svn to create tag 'convolver2-5'.

---
+++ convolver/resource.h
@@ -0,0 +1,31 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by convolver.rc
+//
+#define IDS_PROJNAME                    100
+#define IDR_CONVOLVER                   101
+#define IDS_MIXRANGEERROR               101
+#define IDS_FRIENDLYNAME                102
+#define IDS_DESCRIPTION                 103
+#define IDS_TITLEPROPPAGE               104
+#define IDS_HELPFILEPROPPAGE            105
+#define IDS_DOCSTRINGPROPPAGE           106
+#define IDR_CONVOLVERPROPPAGE           107
+#define IDD_CONVOLVERPROPPAGE           108
+#define IDS_SCALERANGEERROR             109
+#define IDS_DELAYRANGEERROR             109
+#define IDC_DELAYTIME                   201
+#define IDC_MIXLABEL                    202
+#define IDC_WETMIX                      203
+#define IDC_DELAYLABEL                  65535
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        204
+#define _APS_NEXT_COMMAND_VALUE         32768
+#define _APS_NEXT_CONTROL_VALUE         204
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
