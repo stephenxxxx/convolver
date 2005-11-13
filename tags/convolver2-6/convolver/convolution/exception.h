f082c3ae3fae202eb52c4f3b4eba9abf14b4db64
nobody <nobody@61397ecf-7810-0410-9c1b-9d4d218aaa15>
2005-11-13 22:21:02 +0000

    This commit was manufactured by cvs2svn to create tag 'convolver2-6'.

---
+++ tags/convolver2-6/convolver/convolution/exception.h
@@ -0,0 +1,75 @@
+#pragma once
+// Convolver: DSP plug-in for Windows Media Player that convolves an impulse respose
+// filter it with the input stream.
+//
+// Copyright (C) 2005  John Pavel
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+#include <atlbase.h>
+#include <exception>
+#include <string>
+#include <sstream>
+
+
+class convolutionException : public std::exception
+{
+public:
+	convolutionException(std::string diagnostic) : std::exception(), diagnostic_(diagnostic) {}
+
+	virtual ~convolutionException() {}
+
+	virtual const char* what() const
+	{
+		return diagnostic_.c_str();
+	}
+
+protected:
+	std::string diagnostic_;
+};
+
+class configException : public convolutionException
+{
+public:
+	configException(std::string what="") : convolutionException("Problem with config file: " + what) {}
+};
+
+
+class wavfileException : public convolutionException
+{
+public:
+	wavfileException(std::string what, const TCHAR* path, const char* strerror) :
+	  convolutionException("Problem with sound file: " + what + " " + std::string(CT2CA(path)) + " " + std::string(strerror)) {}
+};
+
+
+class filterException : public convolutionException
+{
+public:
+	filterException(std::string what="") : convolutionException("Problem with filter: " + what) {}
+	filterException(HRESULT& hr) : convolutionException("Problem with filter: ")
+	{
+		std::ostringstream ss;
+		ss << diagnostic_ << std::hex << hr;
+		diagnostic_ = ss.str();
+	}
+};
+
+class versionException : public convolutionException
+{
+public:
+	versionException(std::string what) :
+	  convolutionException("Problem getting version information: " + what) {}
+};
---
+++ tags/convolver2-6/convolver/convolution/filter.h
@@ -0,0 +1,78 @@
+#pragma once
+// Convolver: DSP plug-in for Windows Media Player that convolves an impulse respose
+// filter it with the input stream.
+//
+// Copyright (C) 2005  John Pavel
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+#include "convolution\config.h"
+#include "convolution\samplebuffer.h"
+#include "convolution\wavefile.h"
+#include "convolution\waveformat.h"
+#include "convolution\lrint.h"
+#include <vector>
+
+
+const DWORD MAX_FILTER_SIZE = 100000000; // Max impulse size.
+
+class Filter
+{
+public:
+
+	int						nChannels;				// number of channels
+	PartitionedBuffer		coeffs;
+#ifdef LIBSNDFILE
+	SF_INFO					sf_FilterFormat;		// The format of the filter file
+#else
+	WAVEFORMATEXTENSIBLE	wfexFilterFormat;		// The format of the filter file
+#endif
+	int						nPartitions;
+	int						nPartitionLength;		// in blocks (a block contains the samples for each channel)
+	int						nHalfPartitionLength;	// in blocks
+	int						nFilterLength;			// nFilterLength = nPartitions * nPartitionLength
+#ifdef FFTW
+	int						nFFTWPartitionLength;	// 2*(nPaddedPartitionLength/2+1);
+	fftwf_plan				plan;
+	fftwf_plan				reverse_plan;
+	PartitionedBuffer		fft_coeffs;
+#endif
+
+	Filter(TCHAR szFilterFileName[MAX_PATH], const DWORD& nPartitions);
+#ifdef FFTW
+	// // TODO: this does not work because if you have a number of filters of the same length they seem to point to the same thing
+	//~Filter()
+	//{
+	//	fftwf_destroy_plan(plan);
+	//	plan = NULL;
+	//	fftwf_destroy_plan(reverse_plan);
+	//	reverse_plan = NULL;
+	//}
+#endif
+
+
+private:
+#ifdef OOURA
+	// Workspace for the non-simple Ooura routines.  
+	std::vector<DLReal>		w;						// w[0...n/2-1]   :cos/sin table
+	std::vector<int>		ip;						// work area for bit reversal
+													// length of ip >= 2+sqrt(n)
+													// strictly, length of ip >= 
+													//    2+(1<<(int)(log(n+0.5)/log(2))/2).
+#endif
+	//Filter(const Filter& other); // no impl.
+	//void operator=(const Filter& other); // no impl.
+	//Filter(Filter& other);                // discourage use of lvalue Filters
+};
