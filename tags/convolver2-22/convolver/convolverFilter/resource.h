041da79caeb9829963846b6a98edc41875d4473b
nobody <nobody@61397ecf-7810-0410-9c1b-9d4d218aaa15>
2006-01-21 12:04:43 +0000

    This commit was manufactured by cvs2svn to create tag 'convolver2-22'.

---
+++ tags/convolver2-22/convolver/convolverFilter/resource.h
@@ -0,0 +1,51 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by convolverFilter.rc
+//
+#define VERSION_RES_MINOR_VER           0
+#define VERSION_RES_BUILD               0
+#define VER_DEBUG                       0
+#define VERSION_RES_MAJOR_VER           1
+#define IDD_convolverFilterPROP         101
+#define IDC_COMBOPLANNINGRIGOUR         216
+#define IDC_convolverFilter             1000
+#define IDC_GETCONFIG                   1000
+#define IDB_DEFAULT                     1001
+//#define IDC_METHOD                      1002
+//#define IDC_METHOD2                     1003
+//#define IDC_METHOD3                     1004
+//#define IDC_HEIGHT                      1005
+//#define IDC_PARAM1                      1005
+#define IDC_CONFIGFILE                  1005
+//#define IDC_WIDTH                       1006
+//#define IDC_PARAM2                      1006
+#define IDC_ATTENUATION                 1006
+//#define IDC_LENGTH                      1007
+//#define IDC_CROP_TOP                    1007
+#define IDC_NOOFPARTITIONS              1007
+//#define IDC_CROP_LEFT                   1008
+//#define IDC_CROP_HEIGHT                 1009
+//#define IDC_CROP_WIDTH                  1010
+#define IDC_CALCULATEOPTIMUMATTENUATION 1014
+#define IDS_TITLE                       1015
+#define IDS_NOOFPARTITIONS              1016
+#define IDS_STATUS                      1017
+#define IDS_Version                     1018
+//#define IDC_LIST1                       1019
+#define VERSION_RES_LANGUAGE            0x409
+#define VERSION_RES_CHARSET             1252
+#define IDS_STATIC                      -1
+#define IDS_ATTENUATION                 -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_3D_CONTROLS                     1
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1019
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
