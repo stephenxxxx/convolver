530119137ea8ff80b489e4d60c8d6b2bd64445f2
nobody <nobody@61397ecf-7810-0410-9c1b-9d4d218aaa15>
2006-02-01 23:04:18 +0000

    This commit was manufactured by cvs2svn to create tag 'convolver2-24'.

---
+++ tags/convolver2-24/convolver/convolution/filter.cpp
@@ -0,0 +1,473 @@
+// Convolver: DSP plug-in for Windows Media Player that convolves an impulse respose
+// filter it with the input stream.
+//
+// Copyright (C) 2005  John Pavel
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+#include "convolution\ffthelp.h"
+#include "convolution\filter.h"
+
+Filter::Filter(const TCHAR szFilterFileName[MAX_PATH], const WORD& nPartitions, const WORD& nFilterChannel, const DWORD& nSamplesPerSec,
+			   const unsigned int& nPlanningRigour) : 
+nPartitions (nPartitions),
+nSamplesPerSec(nSamplesPerSec)
+{
+#if defined(DEBUG) | defined(_DEBUG)
+	DEBUGGING(3, cdebug << "Filter::Filter " << nPartitions << " " << nSamplesPerSec << std::endl;);
+#endif
+#ifndef LIBSNDFILE
+	HRESULT hr = S_OK;
+#endif
+
+#ifdef UNDEFINED
+	// TODO:: unclear whether this has any effect other than breaking Athlon
+	SIMDFlushToZero();	// Set flush to zero processor mode for speed, with a loss of accuracy
+#endif
+
+	if (nPartitions == 0)
+	{
+		throw filterException("Number of partitions must be at least one", szFilterFileName);
+	}
+
+	// Load the wave file
+#ifdef LIBSNDFILE
+	::ZeroMemory(&sf_FilterFormat_, sizeof(SF_INFO));
+	CWaveFileHandle pFilterWave(szFilterFileName, SFM_READ, &sf_FilterFormat_, nSamplesPerSec); // Throws, if file invalid
+
+	if(sf_FilterFormat_.channels - 1 < nFilterChannel)
+	{
+		throw filterException("Filter channel number too big", szFilterFileName);
+	}
+
+	if(nSamplesPerSec != sf_FilterFormat_.samplerate)
+	{
+		throw filterException("Filter does not have the specified sample rate", szFilterFileName);
+	}
+	nFilterLength_ = sf_FilterFormat_.frames;
+
+#else
+	CWaveFileHandle pFilterWave;
+	hr = pFilterWave->Open( szFilterFileName, NULL, WAVEFILE_READ );
+	if( FAILED(hr) )
+	{
+		throw filterException(hr);
+	}
+
+	// Save filter characteristic, for access by the properties page, etc
+	::ZeroMemory(&wfexFilterFormat_, sizeof(wfexFilterFormat_));
+	wfexFilterFormat_.Format = *pFilterWave->GetFormat();
+
+	if(wfexFilterFormat_.Format.nChannels - 1 < nFilterChannel)
+	{
+		throw filterException("Filter channel number too big");
+	}
+
+	this->nSampleRate = wfexFilterFormat_.Format.nSampleRate;
+
+	WORD wValidBitsPerSample = wfexFilterFormat_.Format.wBitsPerSample;
+	WORD wFormatTag = wfexFilterFormat_.Format.wFormatTag;
+
+	// nBlockAlign should equal nChannels * wBitsPerSample / 8 (bits per byte) for 
+	// WAVE_FORMAT_PCM, WAVE_FORMAT_IEEE_FLOAT and WAVE_FORMAT_EXTENSIBLE
+	nFilterLength_ = pFilterWave->GetSize() / (wfexFilterFormat_.Format.nChannels * wfexFilterFormat_.Format.wBitsPerSample / 8);
+
+	if (wFormatTag == WAVE_FORMAT_EXTENSIBLE)
+	{
+		wfexFilterFormat_ = *(WAVEFORMATEXTENSIBLE*) pFilterWave->GetFormat();  // TODO: Check that this works
+		// wValidBitsPerSample: usually equal to WAVEFORMATEX.wBitsPerSample, 
+		// but can store 20 bits in a 32-bit container, for example
+		wValidBitsPerSample = wfexFilterFormat_.Samples.wValidBitsPerSample;
+
+		if (wfexFilterFormat_.SubFormat == KSDATAFORMAT_SUBTYPE_PCM)
+		{
+			wFormatTag = WAVE_FORMAT_PCM;
+			//// HACK: for Audition -- doesn't work
+			//wfexFilterFormat_.SubFormat == KSDATAFORMAT_SUBTYPE_IEEE_FLOAT;
+			//wFormatTag = WAVE_FORMAT_IEEE_FLOAT; // For Audition-generated files
+		}
+		else
+		{
+			if (wfexFilterFormat_.SubFormat == KSDATAFORMAT_SUBTYPE_IEEE_FLOAT)
+			{
+				wFormatTag = WAVE_FORMAT_IEEE_FLOAT;
+			}
+			else
+			{
+				throw filterException("Subformat not PCM or IEEE Float");
+			}
+		}
+	}
+#endif
+
+	// Setup the filter
+	// A partition will contain half real data, and half zero padding.  Taking the DFT will, of course, overwrite that padding
+	nHalfPartitionLength_ = (nFilterLength_ + nPartitions - 1) / nPartitions;
+
+	OptimalDFT oDFT;	// helper
+	// Check that the filter is not too big ...
+	if ( nHalfPartitionLength_ > oDFT.HalfLargestDFTSize )
+	{
+		throw filterException("Filter too long to handle", szFilterFileName);
+	}
+
+	// .. or filter too small
+	if ( nHalfPartitionLength_ == 0 )
+	{
+		filterException("Filter too short", szFilterFileName);
+	}
+
+	if ( nHalfPartitionLength_ == 1 )
+		nHalfPartitionLength_ = 2; // Make sure that the minimum partition length is 4
+
+	nPartitionLength_ = nHalfPartitionLength_ * 2;
+
+	int nHalfPaddedPartitionLength = oDFT.GetOptimalDFTSize(nHalfPartitionLength_);
+	int nPaddedPartitionLength = nHalfPaddedPartitionLength * 2;
+
+#ifdef OOURA
+	// Initialize the Oooura workspace;
+	ip_.resize(static_cast<int>(sqrt(static_cast<float>(nPaddedPartitionLength)) + 2));
+	ip_[0]=0; // signal the need to initialize
+	w_.resize(nHalfPaddedPartitionLength);	// w_[0..nPaddedPartitionLength/2 - 1]
+#endif
+
+	// Initialise the Filter
+
+	PlanningRigour pr;
+#ifdef FFTW
+	nFFTWPartitionLength_ = 2*(nPaddedPartitionLength/2+1);
+#ifdef ARRAY
+	coeffs_ = SampleBuffer(nPartitions, nFFTWPartitionLength_);
+#else
+	coeffs_ = SampleBuffer(nPartitions, ChannelBuffer(nFFTWPartitionLength_));
+#endif
+	// PATIENT will disable multithreading, if it's not faster
+	if(nPlanningRigour > pr.Measure)
+		fftwf_plan_with_nthreads(2);
+
+	if(nPlanningRigour == pr.TimeLimited)
+		fftwf_set_timelimit(pr.nTimeLimit);
+
+	plan_ =  fftwf_plan_dft_r2c_1d(nPaddedPartitionLength,
+		c_ptr(coeffs_), reinterpret_cast<fftwf_complex*>(c_ptr(coeffs_)),
+		pr.Flag[nPlanningRigour]);
+	reverse_plan_ =  fftwf_plan_dft_c2r_1d(nPaddedPartitionLength, 
+		reinterpret_cast<fftwf_complex*>(c_ptr(coeffs_)), c_ptr(coeffs_),
+		pr.Flag[nPlanningRigour]);
+#else
+#ifdef ARRAY
+	coeffs_ = SampleBuffer(nPartitions, nPaddedPartitionLength);
+#else
+	coeffs_ = SampleBuffer(nPartitions, ChannelBuffer(nPaddedPartitionLength));
+#endif
+#endif
+
+#ifdef LIBSNDFILE
+	std::vector<float> item(sf_FilterFormat_.channels);
+#else
+	assert(wfexFilterFormat_.Format.wBitsPerSample >= wValidBitsPerSample);
+	const DWORD dwSizeToRead = wfexFilterFormat_.Format.wBitsPerSample / 8;  // container size, in bytes
+
+	assert (dwSizeToRead <= 8);
+	std::vector<BYTE> bSample(8,0); // 8 is the biggest sample size (64-bit)
+
+	DWORD dwSizeRead = 0;
+
+	float sample = 0;
+#endif
+
+#if defined(DEBUG) | defined(_DEBUG)
+	float minSample = 0;
+	float maxSample = 0;
+#endif
+
+	// Read the filter file
+	WORD nPartition = 0;
+	DWORD nFrame = 0;					// LibSndFile refers to blocks as frames
+	while (nFrame < nFilterLength_)
+	{
+#ifdef LIBSNDFILE
+		if (1 == pFilterWave.readf_float(&item[0], 1))		// 1 = 1 frame = nChannel items
+		{
+			// Got a frame / block (ie, the items / samples for each channel)
+			// Pick the sample corresponding to the selected channel
+			coeffs_[nPartition][nFrame % nHalfPartitionLength_] = item[nFilterChannel];
+#if defined(DEBUG) | defined(_DEBUG)
+			if (item[nFilterChannel] > maxSample)
+				maxSample = item[nFilterChannel];
+			if (item[nFilterChannel] < minSample)
+				minSample = item[nFilterChannel];
+#endif
+		}
+		else
+		{
+			throw filterException("Failed to read a frame", szFilterFileName);
+		};
+#else
+		for(WORD nChannel = 0; nChannel <= nFilterChannel; ++nChannel)
+		{
+			hr = pFilterWave->Read(&bSample[nChannel], dwSizeToRead, &dwSizeRead);
+
+			if (FAILED(hr))
+			{
+				throw filterException("Failed to read a block", szFilterFileName);
+			}
+
+			if (dwSizeRead != dwSizeToRead) // file corrupt, non-existent, etc
+			{
+				throw filterException("Failed to read a complete block", szFilterFileName);
+			}
+		}
+		// Now convert bSample to the float sample
+		switch (wFormatTag)
+		{
+		case WAVE_FORMAT_PCM:
+			switch (wfexFilterFormat_.Format.wBitsPerSample)	// container size
+			{
+			case 8:
+				{
+					assert(dwSizeToRead == sizeof(BYTE));
+					sample = static_cast<float>(bSample[0] - 128);
+				}
+				break;
+			case 16:
+				{
+					assert(dwSizeToRead == sizeof(INT16));
+					sample = *reinterpret_cast<INT16*>(&bSample[0]);
+				}
+				break;
+			case 24:
+				{
+					assert(dwSizeToRead == 3);  // 3 bytes
+					// Get the input sample
+					int i = bSample[2];
+					i = (i << 8) | bSample[1];
+
+					switch (wValidBitsPerSample)
+					{
+					case 16:
+						break;
+					case 20:
+						i = (i << 4) | (bSample[0] >> 4);
+						break;
+					case 24:
+						i = (i << 8) | bSample[0];
+						break;
+					default:
+						throw filterException("Bit depth for 24-bit container must be 16, 20 or 24", szFilterFileName);
+					}
+					sample = static_cast<float>(i);
+				}
+				break;
+			case 32:
+				{
+					assert(dwSizeToRead == sizeof(INT32));
+
+					//// Get the input sample
+					INT32 i = *reinterpret_cast<INT32 *>(&bSample[0]);
+
+					////sample = (bSample[3] << 24) + (bSample[2] << 16) + (bSample[1] << 8) + bSample[0]; // little-endian. (RIFF)
+					////sample = (bSample[0] << 24) + (bSample[1] << 16) + (bSample[2] << 8) + bSample[3]; // big-endian. (RIFX)
+
+					switch (wValidBitsPerSample)
+					{
+					case 16:
+						i >>= 16;
+						break;
+					case 20:
+						i >>= 12;
+						break;
+					case 24:
+						i >>= 8;
+						break;
+					case 32:
+						break;
+					default:
+						throw filterException("Bit depth for 32-bit container must be 16, 20, 24 or 32", szFilterFileName);
+					}
+					sample = static_cast<float>(i);
+				}
+				break;
+			default:
+				throw filterException("Unsupported PCM sample size", szFilterFileName); // Unsupported it sample size
+			}
+			break;
+
+		case WAVE_FORMAT_IEEE_FLOAT:
+			switch (wfexFilterFormat_.Format.wBitsPerSample)
+			{
+			case 16:
+				throw filterException("16-bit IEEE float sample size not implemented", szFilterFileName);
+			case 24:
+				throw filterException("24-bit IEEE float sample size not implemented", szFilterFileName);
+			case 32:
+				{
+					assert(dwSizeToRead == sizeof(float));
+					sample = *reinterpret_cast<float*>(&bSample[0]);
+				}  // case
+				break;
+			case 64:
+				{
+					assert(dwSizeToRead == sizeof(double));
+					sample = *reinterpret_cast<double*>(&bSample[0]);
+				}  // case
+				break;
+			default:
+				throw filterException("Invalid IEEE float sample size", szFilterFileName); // Unsupported it sample size
+			}
+			break;
+
+		default:
+			throw filterException("Only PCM and IEEE Float file formats supported", szFilterFileName););	// Filter file format is not supported
+		}
+
+		coeffs_[nPartition][nFrame % nHalfPartitionLength_] = sample;
+
+#if defined(DEBUG) | defined(_DEBUG)
+		if (sample > maxSample)
+			maxSample = sample;
+		if (sample < minSample)
+			minSample = sample;
+#endif
+
+		// skip the rest of the block
+		for(;nChannel < wfexFilterFormat_.Format.nChannels ; ++ nChannel)
+		{
+			hr = pFilterWave->Read(&bSample[0], dwSizeToRead, &dwSizeRead);
+
+			if (FAILED(hr))
+			{
+				throw filterException("Failed to read a block", szFilterFileName);
+			}
+
+			if (dwSizeRead != dwSizeToRead) // file corrupt, non-existent, etc
+			{
+				throw filterException("Failed to read a complete block", szFilterFileName);
+			}
+		}
+
+#endif
+
+		++nFrame;
+		if (nFrame % nHalfPartitionLength_ == 0)
+		{
+			// Pad partition
+			for (ChannelBuffer::size_type nPadding = nHalfPartitionLength_; nPadding < nPaddedPartitionLength; ++nPadding)
+			{
+				coeffs_[nPartition][nPadding] = 0;
+			}
+
+			// Take the DFT
+#ifdef FFTW
+			fftwf_execute_dft_r2c(plan_, 
+				c_ptr(coeffs_, nPartition), 
+				reinterpret_cast<fftwf_complex*>(c_ptr(coeffs_, nPartition)));
+#elif defined(OOURA_SIMPLE)
+			rdft(nPaddedPartitionLength, OouraRForward, c_ptr(coeffs_, nPartition));
+#elif defined(OOURA)
+			rdft(nPaddedPartitionLength, OouraRForward, c_ptr(coeffs_, nPartition), &ip_[0], &w_[0]);
+#else
+#error "No FFT package defined"
+#endif
+
+			// Scale here, so that we don't need to do so when convolving
+#ifdef FFTW
+			coeffs_[nPartition] *= static_cast<float>(1.0L / nPaddedPartitionLength);
+#elif defined(OOURA) || defined(SIMPLE_OOURA)
+			coeffs_[nPartition] *= static_cast<float>(2.0L / nPaddedPartitionLength);
+#else
+#error "No FFT package defined"
+#endif
+			++nPartition;
+		}
+	} // while
+
+	// Pad the current partition (if necessary)
+	if (nFrame % nHalfPartitionLength_ != 0)
+	{
+		for (ChannelBuffer::size_type nSample = nFrame % nHalfPartitionLength_; nSample < nPaddedPartitionLength; ++nSample)
+		{
+			coeffs_[nPartition][nSample] = 0;
+		}
+
+		// Take the DFT
+#ifdef FFTW
+		fftwf_execute_dft_r2c(plan_, c_ptr(coeffs_, nPartition), reinterpret_cast<fftwf_complex*>(c_ptr(coeffs_, nPartition)));
+#elif defined(OOURA_SIMPLE)
+		rdft(nPaddedPartitionLength, OouraRForward, c_ptr(coeffs_, nPartition));
+#elif defined(OOURA)
+		rdft(nPaddedPartitionLength, OouraRForward, c_ptr(coeffs_, nPartition), &ip_[0], &w_[0]);
+#else
+#error "No FFT package defined"
+#endif
+
+		// Scale here, so that we don't need to do so when convolving
+#ifdef FFTW
+		coeffs_[nPartition] *= static_cast<float>(1.0L / nPaddedPartitionLength);
+#elif defined(OOURA) || defined(SIMPLE_OOURA)
+		coeffs_[nPartition] *= static_cast<float>(1.0L / nPaddedPartitionLength);
+#else
+#error "No FFT package defined"
+#endif
+		++nPartition;
+	}
+
+	// Zero any further partitions;
+	for (;nPartition < nPartitions; ++nPartition)
+	{
+		coeffs_[nPartition] = 0;
+	}
+
+	// The padded lengths become the actual lengths that we are going to work with
+	nPartitionLength_ = nPaddedPartitionLength;
+	nHalfPartitionLength_ = nHalfPaddedPartitionLength;
+	nFilterLength_ = nPartitions * nPartitionLength_;
+
+#ifdef UNDEFINED
+	// Only works for float. Seems to have no performance benefit
+	for(WORD nPartition=0; nPartition<nPartitions; ++ nPartition)
+	{
+		for(DWORD nSample=0; nSample<nPartitionLength_; ++nSample)
+		{
+			// Kill denormal. Requires 32-bit int
+			const int x = *reinterpret_cast <const int*> (&coeffs_[nPartition][nSample]);
+			const int abs_mantissa = x & 0x007FFFFF;
+			const int biased_exponent = x & 0x7F800000;
+			if (biased_exponent == 0 && abs_mantissa != 0)
+			{
+				coeffs_[nPartition][nSample] = 0;
+			}
+		}
+	}
+#endif
+
+#if defined(DEBUG) | defined(_DEBUG)
+
+	cdebug << "FFT Filter: ";
+	DumpSampleBuffer(coeffs_);
+	cdebug << std::endl;
+
+
+#ifdef LIBSNDFILE
+	cdebug << waveFormatDescription(sf_FilterFormat(), "FFT Filter: ") << std::endl;
+#else
+	cdebug << waveFormatDescription(&wfexFilterFormat_, nFilterLength_, "FFT Filter:") << std::endl;
+#endif
+
+	cdebug << "minSample " << minSample << ", maxSample " << maxSample << std::endl;
+#endif
+
+}
---
+++ tags/convolver2-24/convolver/convolution/samplebuffer.h
@@ -0,0 +1,477 @@
+#pragma once
+// Convolver: DSP plug-in for Windows Media Player that convolves an impulse respose
+// filter it with the input stream.
+//
+// Copyright (C) 2005  John Pavel
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+
+#include "convolution\config.h"
+
+// Vector is slow when debugging
+#include <malloc.h>
+//#include <windows.h>    // for ZeroMemory, CopyMemory, etc
+#include <iterator>
+#include <vector>
+#include <algorithm>
+
+// The following undefs are needed to avoid conflicts
+#undef min
+#undef max
+
+#ifdef FASTARRAY
+
+//#ifdef FFTW
+//#define ALIGNED_MALLOC(bytes) fftwf_malloc(bytes)
+//#define ALIGNED_FREE(ptr) fftwf_free(ptr)
+#if defined(__ICC) || defined(__INTEL_COMPILER)
+#define ALIGNED_MALLOC(bytes) _mm_malloc(bytes, 16)
+#define ALIGNED_FREE(ptr) _mm_free(ptr)
+#else // Microsoft
+#define ALIGNED_MALLOC(bytes) _aligned_malloc(bytes, 16)
+#define ALIGNED_FREE(ptr) _aligned_free(ptr)
+#endif
+
+
+// Use vectors of vecors/arrays, as we need aligned data at the base.
+// Using projections onto a 1d array would not guarantee alignment other than of the first row
+
+// A simple aligned array
+template <class T>
+class AlignedArray
+{
+public:
+	typedef T					value_type;
+	typedef std::size_t			size_type;
+
+	// Constructors
+	AlignedArray() :   first_(NULL), size_(0), bsize_(0)  {}
+
+	explicit AlignedArray(const size_type& n) : first_(NULL), size_(0), bsize_(0)
+	{
+		if (n != 0) {
+			first_ = static_cast<T*>(ALIGNED_MALLOC(n * sizeof(T)));
+			if (first_ == NULL) {
+				throw std::bad_alloc();
+			}
+			size_  = n;
+			bsize_ = n * sizeof(T);
+		}
+		else {
+			first_ = NULL;
+			size_ = 0;
+			bsize_ = 0;
+		}   
+	}
+
+	// Destructor
+	virtual ~AlignedArray()
+	{   
+		ALIGNED_FREE(first_);
+		first_ = NULL;
+		size_ = 0;
+		bsize_ = 0;
+	}
+
+protected:
+
+#if defined(__ICC) || defined(__INTEL_COMPILER)
+	_declspec(align(16)) T* restrict first_;
+#else
+	T* restrict first_;
+#endif
+	size_type size_;        // length in elements
+	size_type bsize_;       // length in bytes
+
+private:
+
+	// No copying
+	AlignedArray(const AlignedArray<T>&);
+	AlignedArray& operator=(const AlignedArray<T>&);
+};
+
+// comparisons
+template<class T>
+bool operator== (const AlignedArray<T>& x, const AlignedArray<T>& y);
+template<class T>
+bool operator< (const AlignedArray<T>& x, const AlignedArray<T>& y);
+template<class T>
+bool operator!= (const AlignedArray<T>& x, const AlignedArray<T>& y);
+template<class T>
+bool operator> (const AlignedArray<T>& x, const AlignedArray<T>& y);
+template<class T>
+bool operator<= (const AlignedArray<T>& x, const AlignedArray<T>& y);
+template<class T>
+bool operator>= (const AlignedArray<T>& x, const AlignedArray<T>& y);
+
+// global swap()
+template<class T>
+inline void swap (AlignedArray<T>& x, AlignedArray<T>& y);
+
+template <typename T> 
+struct FastArray : public AlignedArray<T>
+{
+
+	// type definitions
+
+	typedef typename AlignedArray<T>::value_type		value_type;
+
+	typedef value_type*									pointer;
+	typedef const value_type*							const_pointer;
+	typedef value_type*									iterator;
+	typedef const value_type*							const_iterator;
+	typedef value_type&									reference;
+	typedef const value_type&							const_reference;
+	typedef typename AlignedArray<T>::size_type			size_type;
+	typedef std::ptrdiff_t								difference_type;
+
+	// iterator support
+	iterator begin() { return first_; }
+	const_iterator begin() const { return first_; }
+	iterator end() { return first_ + size_; }
+	const_iterator end() const { return first_ + size_; }
+
+	// reverse iterator support
+	typedef std::reverse_iterator<iterator> reverse_iterator;
+	typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
+
+	reverse_iterator rbegin() { return reverse_iterator(end()); }
+	const_reverse_iterator rbegin() const
+	{
+		return const_reverse_iterator(end());
+	}
+	
+	reverse_iterator rend() { return reverse_iterator(begin()); }
+	const_reverse_iterator rend() const
+	{
+		return const_reverse_iterator(begin());
+	}
+
+	// Basic constructors
+	FastArray() : AlignedArray<T>() {}
+	explicit FastArray(const size_type n) : AlignedArray<T>(n)
+	{ 
+		// TODO: we probably don't need to do this
+		std::uninitialized_fill_n(first_, size_, 0);
+	}
+	FastArray(const_reference x, const size_t n) : AlignedArray<T>(n)
+	{ 
+		std::uninitialized_fill_n(first_, size_, x);
+	}
+	FastArray(const_pointer p, const size_t n) : AlignedArray<T>(n)
+	{
+		std::uninitialized_copy(p, p + n, first_);
+	}
+	FastArray(const FastArray<T>& other) : AlignedArray<T>(other.size_)
+	{
+		std::uninitialized_copy(other.first_, other.first_ + other.size_,
+			first_);
+	}
+
+	virtual ~FastArray() {};
+
+	// Element access
+
+	// operator[]
+	reference restrict operator[](size_type n)
+	{
+		assert(n >= 0 && n < size_);
+		return *(first_ + n);
+	}
+
+	const_reference operator[](size_type n) const
+	{
+		assert(n >= 0 && n < size_);
+		return *(first_ + n);
+	}
+
+	// at() with range check
+	reference restrict at(size_type n)
+	{
+		rangecheck(i);
+		return *(first_ + n);
+	}
+
+	const_reference at(size_type i) const
+	{
+		rangecheck(i);
+		return *(first_ + n);
+	}
+
+	// front() and back()
+	reference front() { return *first_; }
+	const_reference front() const { return *first_; }
+	reference back() 
+	{
+		assert(!empty());
+		return *(first_ + size_ - 1);
+	}
+	const_reference back() const
+	{
+		assert(!empty());
+		return *(first_ + size_ - 1);
+	}
+
+	// size is constant
+	size_type size() const { return size_; }
+	size_type bsize() const { return bsize_; }
+	bool empty() const { return size_ == 0; }
+	size_type max_size() const { return size_; }
+	size_type static_size() const { return size_; }
+
+	// For use with C interface
+	pointer restrict c_ptr() const
+	{
+		return empty() ? NULL : first_;
+	}
+
+	// swap (note: linear complexity in N, constant for given instantiation)
+	//void swap (FastArray<T>& y) {
+	//	std::swap_ranges(begin(),end(),y.begin());
+	//}
+
+	// Helper for =
+	void swap(FastArray& x)
+	{
+		std::swap(first_, x.first_);
+		std::swap(size_, x.size_);
+		std::swap(bsize_, x.bsize_);
+	}
+
+	// direct access to data (read-only)
+	const_pointer data() const { return first_; }
+
+	// use array as C array (direct read/write access to data)
+	pointer data() { return first_; }
+
+	// assign one value to all elements
+	void assign (const T& x)
+	{
+		std::fill_n(begin(), size(), x);
+	}
+
+	// Basic assignment.  Note that assertions are stricter than necessary, as unequally-size
+	// arrays will be assigned correctly
+	FastArray<T>& operator=(const FastArray<T>& other)
+	{
+		assert(size_ == 0 || size_ == other.size_);
+		if (this != &other)
+		{
+			FastArray<T> temp(other);
+			swap(temp);
+		}
+		return *this;
+	}
+
+	// assignment with type conversion
+	template <typename T2>
+	FastArray<T>& operator= (const FastArray<T2>& rhs)
+	{
+#ifdef FFTW
+		assert(size() == rhs.size() || size() == 2*(rhs.size()/2+1));
+#else
+		assert(size() == rhs.size());
+#endif
+		assert(size() == rhs->size()));
+		std::copy(rhs.begin(),rhs.end(), begin());
+		return *this;
+	}
+
+	// Scalar assignment
+
+	FastArray<T>& operator=(const_reference x)
+	{
+		std::uninitialized_fill_n(first_, size_, x);
+//		if(x == 0)
+//		{
+//			::ZeroMemory(first_, bsize_);
+//		}
+//		else
+//		{
+//			const size_type ss = size_;						// Improve chances of vectorization
+//#pragma loop count(65536)
+//#pragma ivdep
+//#pragma vector aligned
+//			for (size_type i = 0; i < ss; ++i)
+//				(*this)[i] = static_cast<T>(x);             // TODO: optimize int->float
+//		}
+		return *this;
+	}
+
+	// Scalar computed assignment.
+	FastArray<T>& operator*= (const_reference x)
+	{
+		if(x != static_cast<T>(1))
+		{
+			const size_type ss = size_;
+#pragma loop count(65536)
+#pragma ivdep
+#pragma vector aligned
+			for (size_type i = 0; i < ss; ++i)
+				(*this)[i] *= x;
+		}
+		return *this;
+	}
+
+	FastArray<T>& operator+= (const_reference x)
+	{
+		if(x != 0)
+		{
+//			const size_type ss = size_;
+#pragma loop count(65536)
+#pragma ivdep
+#pragma vector aligned
+			for (size_type i = 0; i < ss; ++i)
+				(*this)[i] += x;
+		}
+		return *this;
+	}
+
+	template <typename T2>
+		FastArray<T>& operator+= (const FastArray<T2>& rhs)
+	{
+#ifdef FFTW
+		assert(size() == rhs.size() || size() == 2*(rhs.size()/2+1));
+#else
+		assert(size() == rhs.size());
+#endif
+		// Optimization
+		const size_type rhs_size = rhs.size();
+#pragma loop count(65536)
+#pragma ivdep
+#pragma vector aligned
+		for (size_type i = 0; i < rhs_size; ++i)
+			(*this)[i] += rhs[i];
+
+		return *this;
+	}
+
+	// Member functions
+
+	void shiftleft(const size_type n)
+	{
+		//std::copy(begin() + n, begin() + 2 * n - 1, begin());
+		::MoveMemory(first_, first_ + n, n * sizeof(T));
+	}
+
+private:
+	// check range (may be private because it is static)
+	static void rangecheck (size_type n)
+	{
+		if (n < 0 || n >= size())
+		{ 
+			throw std::out_of_range("FastArray<>: index out of range");
+		}
+	}
+};
+
+// The following is unused, but could provide a basis for using std::vector instead of FastArray as a basis
+
+//COTD Entry by James Johnson [kmeson@telocity.com]
+///////////////////////////////////////////////////////////////////////////////
+// Class:           aligned_BlockAlloc
+// Base Classes:    std::allocator<T>
+// Type:            template class
+// Inheritance:     public
+// Desc:            implements a block aligned allocator. Allocation occurs on
+//                  T_ALIGNMENT boundary. This boundary must be a power of 2.
+//                  The minimum block size allocated is T_BLOCKSIZE and must be
+//                  non-ZERO. Allocations smaller than	T_BLOCKSIZE are rounded
+//                  up to the nears multiple of T_BLOCKSIZE.
+//
+//                  Default alignment is 16 bytes
+//                  Default block size is 8 bytes
+//
+// Usage:
+//
+//    std::vector<float,aligned_BlockAlloc<float> >	vecFloatAlign16by8;
+//    vecFloatAlign16by8.resize(1024,1.0f)
+//
+///////////////////////////////////////////////////////////////////////////////
+template <typename T,
+           unsigned long T_ALIGNMENT=16,
+           unsigned long T_BLOCKSIZE=8>
+class aligned_BlockAlloc : public std::allocator<T>
+{
+private:
+     typedef std::allocator<T>  BASE_TYPE;
+
+public:
+     aligned_BlockAlloc() {}
+     aligned_BlockAlloc& operator=(const aligned_BlockAlloc& r)
+	 {
+         BASE_TYPE::operator=(r);
+         return *this;
+     }
+
+     pointer allocate(size_type n, const void *hint){
+         pointer p = NULL;
+         unsigned long byteCount = sizeof(T) * n;
+         unsigned long byteCountLeft = byteCount % T_BLOCKSIZE;
+         if(byteCountLeft)
+		 {
+             byteCount += T_BLOCKSIZE -  byteCountLeft;
+         }
+         if(!hint
+			 ){
+             p = reinterpret_cast<pointer>(_aligned_malloc(byteCount,T_ALIGNMENT));
+         }
+		 else
+		 {
+             p = reinterpret_cast<pointer>(_aligned_realloc((void*)hint,byteCount,T_ALIGNMENT));
+         }
+         return p;
+     }
+
+     void deallocate(pointer p, size_type n)
+	 {
+         _aligned_free(p);
+     }
+
+     void construct(pointer p, const T &val)
+	 {
+         new(p) T(val);
+     }
+
+     void destroy(pointer p)
+	 {
+         p->~T();
+     }
+};
+
+
+
+typedef FastArray<float> ChannelBuffer;
+typedef std::vector<ChannelBuffer> SampleBuffer;
+typedef std::vector<SampleBuffer> PartitionedBuffer;
+
+// float must be the same as the FastArray base type
+extern float* restrict c_ptr(const ChannelBuffer& x);
+extern float* restrict c_ptr(const SampleBuffer& x, const ChannelBuffer::size_type row=0);
+extern float* restrict c_ptr(const PartitionedBuffer& x, const SampleBuffer::size_type row=0, const ChannelBuffer::size_type column=0);
+
+// zero all the elements
+extern void Zero (SampleBuffer& x);
+extern void Zero (PartitionedBuffer& x);
+
+#endif
+
+
+#if defined(DEBUG) | defined(_DEBUG)
+void DumpSampleBuffer(const SampleBuffer& buffer);
+void DumpChannelBuffer(const ChannelBuffer& buffer);
+void DumpPartitionedBuffer(const PartitionedBuffer& buffer);
+#endif
